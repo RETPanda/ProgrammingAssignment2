afba64ad20aa01079b1425ffa8a4aac3c819c466
RETPanda <RETPanda@users.noreply.github.com>
2015-07-27 00:55:18 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix:Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
 
-## Write a short comment describing this function
 
+## function creates a matrix that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
